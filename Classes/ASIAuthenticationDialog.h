54804c19c3661e2f482d1ae7930135663fd785c0
Gerald Kaszuba <gerald.kaszuba@gmail.com>
2010-06-10 11:48:37 +1000

    fixed for library and using reachability2

--- Classes/ASIAuthenticationDialog.h
+++ Classes/ASIAuthenticationDialog.h
@@ -6,7 +6,7 @@
 //  Copyright 2009 All-Seeing Interactive. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 @class ASIHTTPRequest;
 
 typedef enum _ASIAuthenticationType {
